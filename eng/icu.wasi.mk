d717f6117acd1daa5243593df3b37ee296f7292b
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-01-18 16:50:41 +0100

    update compiler config

--- eng/icu.wasi.mk
+++ eng/icu.wasi.mk
@@ -5,7 +5,7 @@ endif
 CONFIGURE_COMPILER_FLAGS += \
         CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
         CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
-        CC="$(WASI_SDK_PATH)/bin/clang++ --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot" \
-        --host=x86_64-pc-linux-gnu --build=wasm32
+        CC="$(WASI_SDK_PATH)/bin/clang --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot" \
+        --host=x86_64-pc-linux-gnu --build=wasm32-wasi
 check-env:
   :
