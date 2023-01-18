24885c9f07f150e473d400ee85d8b1217d298347
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-01-18 17:00:47 +0100

    update

--- eng/icu.wasi.mk
+++ eng/icu.wasi.mk
@@ -7,6 +7,6 @@ CONFIGURE_COMPILER_FLAGS += \
         CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
         CC="$(WASI_SDK_PATH)/bin/clang --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot" \
 		CXX="$(WASI_SDK_PATH)/bin/clang++ --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot" \
-        --host=x86_64-pc-linux-gnu --build=wasm32-wasi
+        --host=x86_64-pc-linux-gnu --build=wasm32
 check-env:
   :
