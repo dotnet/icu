32e532bc808cdc25414f287461fb6144f796ba02
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-01-18 16:16:39 +0100

    update compiler_flags

--- eng/icu.wasi.mk
+++ eng/icu.wasi.mk
@@ -3,14 +3,9 @@ ifeq ($(WASM_ENABLE_THREADS),true)
 endif
 
 CONFIGURE_COMPILER_FLAGS += \
-       CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
-       CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
-       CC="$(WASI_SDK_PATH)/bin/clang --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot"
-
-CONFIGURE_COMPILER_FLAGS += \
         CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
         CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
         CC="$(WASI_SDK_PATH)/bin/clang --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot" \
-        --host=arm-apple-darwin --build=wasm32
+        --host= x86_64-pc-linux-gnu --build=wasm32
 check-env:
   :
