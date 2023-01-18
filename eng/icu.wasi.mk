2b441a634fc9733de5fa19305b0a3bfaf970a8ec
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-01-18 15:08:15 +0100

    update wasi clang paths

--- eng/icu.wasi.mk
+++ eng/icu.wasi.mk
@@ -3,8 +3,9 @@ ifeq ($(WASM_ENABLE_THREADS),true)
 endif
 
 CONFIGURE_COMPILER_FLAGS += \
-	CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
-	CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)"
+       CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
+       CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
+       CC="$(WASI_SDK_PATH)/bin/clang --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot"
 
 check-env:
   :
