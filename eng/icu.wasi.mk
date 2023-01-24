ea5566615b8adccd7669b573321209cb783dca25
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-01-24 15:37:51 +0100

    Refactoring

--- eng/icu.wasi.mk
+++ eng/icu.wasi.mk
@@ -19,11 +19,11 @@ ifeq ($(WASM_ENABLE_THREADS),true)
 endif
 
 CONFIGURE_COMPILER_FLAGS += \
-        CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
-        CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
-        CC="$(WASI_SDK_PATH)/bin/clang --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot" \
-		CXX="$(WASI_SDK_PATH)/bin/clang++ --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot" \
-		--host=$(HOST_PLATFORM) --build=wasm32
+	CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
+	CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(THREADS_FLAG) $(ICU_DEFINES)" \
+	CC="$(WASI_SDK_PATH)/bin/clang --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot" \
+	CXX="$(WASI_SDK_PATH)/bin/clang++ --sysroot=$(WASI_SDK_PATH)/share/wasi-sysroot" \
+	--host=$(HOST_PLATFORM) --build=wasm32
 
 check-env:
-  :
+	:
