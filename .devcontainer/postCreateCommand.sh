3036bc2e3e858ca544cb8ed526bbd4653f28c147
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-01-25 11:41:30 +0100

    Remove unused code

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,3 @@
-if [[ "$__TargetOS" == Browser ]]; then
 EMSDK_PATH=$PWD/artifacts/emsdk
 rm -rf $EMSDK_PATH
 git clone https://github.com/emscripten-core/emsdk.git $EMSDK_PATH
@@ -7,10 +6,3 @@ cd $EMSDK_PATH && ./emsdk install $EMSCRIPTEN_VERSION
 cd $EMSDK_PATH && ./emsdk activate $EMSCRIPTEN_VERSION
 # ready to build, e.g.:
 # ./build.sh /p:TargetOS=Browser /p:TargetArchitecture=wasm /p:IcuTracing=true
-elif [[ "$__TargetOS" == Wasi ]]; then
-WASI_SDK_PATH=$PWD/artifacts/wasi-sdk
-rm -rf $WASI_SDK_PATH
-git clone https://github.com/WebAssembly/wasi-sdk.git $WASI_SDK_PATH
-# ready to build, e.g.:
-# ./build.sh /p:TargetOS=Wasi /p:TargetArchitecture=wasm /p:IcuTracing=true
-fi
