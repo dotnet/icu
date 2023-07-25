b14645f5c8c741d951caa886ca189d0a9e00ed1a
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-07-25 15:27:33 +0200

    Get 3 letter country and language codes

--- icu/icu4c/source/common/unicode/uloc.h
+++ icu/icu4c/source/common/unicode/uloc.h
@@ -526,6 +526,17 @@ uloc_getISO3Language(const char* localeID);
 
 
 /**
+ * Gets the 3 letter ISO language code for the specified 2 letter language code.
+ *
+ * @param langCode Iso639LanguageTwoLetterName
+ * @return language the ISO language code for langCode
+ * @stable ICU 2.0
+ */
+U_CAPI const char* U_EXPORT2
+uloc_getISO3LanguageByLangCode(const char* langCode);
+
+
+/**
  * Gets the ISO country code for the specified locale.
  *
  * @param localeID the locale to get the ISO country code with
@@ -534,6 +545,16 @@ uloc_getISO3Language(const char* localeID);
  */
 U_CAPI const char* U_EXPORT2
 uloc_getISO3Country(const char* localeID);
+
+/**
+ * Gets the 3 letter ISO country code for the specified 2 letter country code.
+ *
+ * @param countryCode Iso3166CountryName
+ * @return country the ISO country code for countryCode
+ * @stable ICU 2.0
+ */
+U_CAPI const char* U_EXPORT2
+uloc_getISO3CountryByCountryCode(const char* countryCode);
 
 /**
  * Gets the Win32 LCID value for the specified locale.
